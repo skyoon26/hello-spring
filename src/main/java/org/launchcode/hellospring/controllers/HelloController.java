4618022ba070c194fa5b4c147ea3345cf36d3dac
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-04-25 12:59:07 -0500

    Update form handler with language options and add associated methods to return appropriate user greeting

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -21,7 +21,6 @@ public class HelloController {
         return "Goodbye, Spring!";
     }
 
-    //lives /hello/hello
     // Handles request of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public String helloWithQueryParam(@RequestParam String name) {
@@ -39,12 +38,47 @@ public class HelloController {
     public String helloForm() {
         return "<html>" +
                     "<body>" +
-                        "<form action='hello' method='post'>" + // submit a request to /hello
-                            "<input type='text' name='name'>" +
-                            "<input type='submit' value='Greet me!'>" +
+                        "<form action='/hello/greeting' method='post'>" + // submit a request to /hello
+                        "<input type='text' name='name'>" +
+                        "<select name='language'>" +
+                            "<option value='english'>English</option>" +
+                            "<option value='french'>French</option>" +
+                            "<option value='spanish'>Spanish</option>" +
+                            "<option value='japanese'>Japanese</option>" +
+                            "<option value='german'>German</option>" +
+                        "</select>" +
+                        "<input type='submit' value='Greet me!'>" +
                         "</form>" +
                     "</body>" +
                 "</html>";
+    }
+
+    @RequestMapping(value="greeting", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
+    }
+
+    public static String createMessage(String name, String language) {
+        String greeting = "";
+
+        if (language.equals("english")) {
+            greeting = "Hello";
+        } else if (language.equals("french")) {
+            greeting = "Bonjour";
+        } else if (language.equals("spanish")) {
+            greeting = "Hola";
+        } else if (language.equals("japanese")) {
+            greeting = "Konnichiwa";
+        } else if (language.equals("german")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + ", " + name + "!";
     }
 
 }
